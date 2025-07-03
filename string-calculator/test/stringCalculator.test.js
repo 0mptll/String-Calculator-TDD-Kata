de83cb33abe84b063caebac8a1498593c9613e79
0mptll <omp9979989@gmail.com>
2025-07-03 21:03:44 +0530

    test: return value itself for single number input

--- string-calculator/test/stringCalculator.test.js
+++ string-calculator/test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/stringCalculator');
 test('return 0 for empty string', () => {
     expect(add('')).toBe(0);
 });
+
+test('return number for single value', () => {
+    expect(add('7')).toBe(7);
+});
