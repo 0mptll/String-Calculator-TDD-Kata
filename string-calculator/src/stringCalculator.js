e3555486eed3c6eefe4757db8cfea009c020799b
0mptll <omp9979989@gmail.com>
2025-07-04 21:22:49 +0530

    feat: support custom single-character delimiters

--- string-calculator/src/stringCalculator.js
+++ string-calculator/src/stringCalculator.js
@@ -3,8 +3,20 @@ function add(num)
     if(num === "")
         return 0;
     
-    const parts = num.split(/,|\n/);
-    return parts.reduce((sum, n) => sum + parseInt(n), 0);
+    let delimiter = /,|\n/;
+
+    if (num.startsWith('//')) {
+        const [delim, rest] = num.split('\n');
+        const rawDelimiter = delim.slice(2);
+        const escaped = rawDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+        delimiter = new RegExp(escaped);
+        num = rest;
+    }
+
+    const parts = num.split(delimiter);
+    return parts.reduce((sum, n) => sum + parseInt(n, 10), 0);
+
+    
 }
 
 module.exports = { add };
