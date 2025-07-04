941a35fecceb41898367070e83cc02574cf51998
0mptll <omp9979989@gmail.com>
2025-07-04 21:06:25 +0530

    test: support newline as valid delimiter

--- string-calculator/test/stringCalculator.test.js
+++ string-calculator/test/stringCalculator.test.js
@@ -15,3 +15,7 @@ test('return sum of two numbers separated by comma', () => {
 test('return sum of multiple numbers', () => {
   expect(add('1,2,3,4')).toBe(10);
 });
+
+test('handles newlines between numbers', () => {
+  expect(add('1\n2,3')).toBe(6);
+});
