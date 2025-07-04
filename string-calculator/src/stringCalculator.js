31e32647e0700c0f198740c9761cea36847b653a
0mptll <omp9979989@gmail.com>
2025-07-04 21:05:14 +0530

    feat: support newline along with comma as delimiter

--- string-calculator/src/stringCalculator.js
+++ string-calculator/src/stringCalculator.js
@@ -3,7 +3,7 @@ function add(num)
     if(num === "")
         return 0;
     
-    const parts = num.split(',');
+    const parts = num.split(/,|\n/);
     return parts.reduce((sum, n) => sum + parseInt(n), 0);
 }
 
